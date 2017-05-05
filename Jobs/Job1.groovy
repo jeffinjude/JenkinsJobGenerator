13c0b7610b63e009e802d2db18ac18787722ce24
jeffinjude <jeffinjude@gmail.com>
2017-05-05 11:53:41 +0000

    Update Job1.groovy

--- Jobs/Job1.groovy
+++ Jobs/Job1.groovy
@@ -2,11 +2,20 @@ def jobName = 'GeneratedJob1'
 def gitUrl = 'https://github.com/jeffinjude/ObjectsRepo.git'
 def branchName = 'master'
 job(jobName) {
+	label('ubuntu-slave-node')
+	
 	parameters {
 		stringParam('MESSAGE', 'Hello world!') 
 	}
 	scm {
-        	git(gitUrl, branchName)
+        	git {
+			remote {
+				github('jeffinjude/ObjectsRepo.git')
+			}
+			configure { scmNode ->
+				scmNode / gitTool('ubuntu-slave-git')
+			}
+		}
 	}
 	
 	steps {
