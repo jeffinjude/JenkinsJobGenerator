20ee5c75958e8c22c516b082621e5c9fc34bb410
Sowmya Mohanan <sowmya.mohanan@cognizant.com>
2017-05-05 13:05:34 +0000

    added a dsl script to create a pipeline job

---
+++ Jobs/Job2.groovy
@@ -0,0 +1,23 @@
+def jobName = 'GeneratedJob2'
+
+pipelineJob(jobName) {
+    logRotator{
+        numToKeep 30
+    }
+    definition {
+        cps {
+            sandbox()
+            script("""
+                node {
+                    stage 'Stage 1'
+                    echo 'Entering stage 1'
+                    stage 'Stage 2'
+                    echo 'Entering stage 2'
+					stage 'Stage 3'
+                    echo 'Entering stage 3'
+                    
+                }
+            """.stripIndent())
+        }
+    }
+}
