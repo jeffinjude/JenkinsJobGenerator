1012f5de9ac5faaba074c5b67a69dc84bcc1ef75
jeffinjude <jeffinjude@gmail.com>
2017-05-05 07:12:46 +0000

    Update Job1.groovy

--- Jobs/Job1.groovy
+++ Jobs/Job1.groovy
@@ -1,6 +1,6 @@
 def jobName = 'GeneratedJob1'
 def gitUrl = 'https://github.com/jeffinjude/ObjectsRepo.git'
-def branchName = master
+def branchName = 'master'
 job(jobName) {
     scm {
             git(gitUrl, branchName)
