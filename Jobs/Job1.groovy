e8b93eb7a054686a02f156ac7d9a74fc8bc6ac22
jeffinjude <jeffinjude@gmail.com>
2017-05-05 07:55:06 +0000

    Update Job1.groovy

--- Jobs/Job1.groovy
+++ Jobs/Job1.groovy
@@ -2,11 +2,14 @@ def jobName = 'GeneratedJob1'
 def gitUrl = 'https://github.com/jeffinjude/ObjectsRepo.git'
 def branchName = 'master'
 job(jobName) {
-    scm {
-            git(gitUrl, branchName)
-    }
+	parameters {
+		stringParam('MESSAGE', 'Hello world!') 
+	}
+	scm {
+        	git(gitUrl, branchName)
+	}
 	
 	steps {
-        shell('echo "Hello, world!"')
-    }
+		shell('echo ${MESSAGE}')
+	}
 }
