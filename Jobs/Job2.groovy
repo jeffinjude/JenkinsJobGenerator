f8f864a5b6ec140f844a290d8de817819c764d7e
jeffinjude <jeffinjude@gmail.com>
2017-05-08 06:13:10 +0000

    Update Job2.groovy

--- Jobs/Job2.groovy
+++ Jobs/Job2.groovy
@@ -7,17 +7,17 @@ pipelineJob(jobName) {
     definition {
         cps {
             sandbox()
-            script("""
+            script("
                 node {
                     stage 'Stage 1'
                     echo 'Entering stage 1'
                     stage 'Stage 2'
                     echo 'Entering stage 2'
-					stage 'Stage 3'
+		    stage 'Stage 3'
                     echo 'Entering stage 3'
                     
                 }
-            """.stripIndent())
+            ".stripIndent())
         }
     }
 }
